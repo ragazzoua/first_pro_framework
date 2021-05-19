9825c93fd8738f00f8c69bda6b7ab475e5d485c2
Serhii Miroshnychenko <sergey.miroshnychenko@gmail.comgit config --global user.email sergey.miroshnychenko@gmail.com>
2021-05-19 21:55:32 +0300

    credential updated. Login in the application

--- src/test/java/app_manager/Application.java
+++ src/test/java/app_manager/Application.java
@@ -3,7 +3,7 @@ package app_manager;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.WebDriver;
-import pages.MainPage;
+import pages.LoginPage;
 import properties.ProjectProperties;
 
 import java.util.concurrent.TimeUnit;
@@ -13,7 +13,7 @@ import static properties.ProjectProperties.setMainSettings;
 public class Application {
     private WebDriver driver;
     private String browser;
-    private MainPage mainPage;
+    private LoginPage loginPage;
 
     public Application(String browser) {
         this.browser = browser;
@@ -25,7 +25,7 @@ public class Application {
         driver = WebDriverRunner.driver().getWebDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        mainPage = new MainPage(driver);
+        loginPage = new LoginPage(driver);
     }
 
 
@@ -37,7 +37,7 @@ public class Application {
         Selenide.switchTo().alert().accept();
     }
 
-    public MainPage getMainPage() {
-        return mainPage;
+    public LoginPage getLoginPage() {
+        return loginPage;
     }
 }
--- src/test/java/ui_tests/main_page_tests/FirstTest.java
+++ src/test/java/ui_tests/main_page_tests/FirstTest.java
@@ -1,6 +1,7 @@
 package ui_tests.main_page_tests;
 
 import org.testng.annotations.Test;
+import properties.ProjectProperties;
 import ui_tests.BaseTest;
 
 public class FirstTest extends BaseTest {
@@ -8,6 +9,6 @@ public class FirstTest extends BaseTest {
 
     @Test
     public void openPage(){
-        application.getMainPage().closePopupIfVisible();
+        application.getLoginPage().login(ProjectProperties.getProperties().getProperty("name"), ProjectProperties.getProperties().getProperty("password"));
     }
 }
